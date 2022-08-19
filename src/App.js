c69e690e4fcbb128039c91b5c2f1ed6d5514898e
ricardomolina <ricardomolina317@gmail.com>
2022-08-18 23:57:29 -0300

    Primera clase de React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let imagen = "mcdonalds.png";
+  let titulo = "Doble Cuarto de Libra";
+  let calorias = "771 kcal";
+  let descripcion = "Imaginá la sensación del clásico Cuarto de Libra. Imaginalo con un medallón de deliciosa carne 100% vacuna, queso cheddar, cebolla, kétchup y mostaza ¿Listo? Ahora duplicá esa sensación. Ya tenés el Doble Cuarto en la cabeza.";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+        <div className="col-md-4 offset-md-2">
+          <img src={imagen} alt={titulo} className="img-fluid" />
+        </div>
+        <div className="col-md-4 ">
+          <h1>{titulo}</h1>
+          <p>{calorias}</p>
+          <p>{descripcion}</p>
+        </div>
+      </div>
     </div>
   );
 }
